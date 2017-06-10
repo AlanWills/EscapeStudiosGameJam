c1ebe3a056360691f4842bcc754becd776afd349
Alan Wills <alawills@googlemail.com>
2017-06-10 20:39:13 +0100

    Working on audio for left brain right brain

---
+++ Assets/Scripts/CustomSteps/PlayAudio.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : CustomStep
+{
+    public AudioSource AudioSource;
+    
+    public override void OnSuccess()
+    {
+        AudioSource.Play();
+    }
+
+    public override void OnFailure()
+    {
+    }
+}
