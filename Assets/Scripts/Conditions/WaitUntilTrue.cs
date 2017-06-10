53caff8b83ab00a664c574195f08275ac213e9ff
Alan Wills <alawills@googlemail.com>
2017-06-10 23:13:33 +0100

    Working on chicken sad level

---
+++ Assets/Scripts/Conditions/WaitUntilTrue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitUntilTrue : ConditionScript
+{
+    public bool IsTrue;
+
+    protected override bool Condition()
+    {
+        return IsTrue;
+    }
+}
