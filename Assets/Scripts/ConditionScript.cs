4809175daafcd4c63e031c1f970d3571e99bff99
Alan Wills <alawills@googlemail.com>
2017-06-10 09:03:18 +0100

    Working on prototype first level

---
+++ Assets/Scripts/ConditionScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TransitionToSceneScript))]
+public abstract class ConditionScript : MonoBehaviour
+{
+    private GameObject correctSymbol;
+    private TransitionToSceneScript transitionScript;
+
+	// Use this for initialization
+	void Start()
+    {
+        correctSymbol = GameObject.Find("CorrectSymbol");
+        Debug.Assert(correctSymbol != null, "Correct symbol null");
+        correctSymbol.SetActive(false);
+
+        transitionScript = GetComponent<TransitionToSceneScript>();
+        Debug.Assert(transitionScript != null, "Transition script null");
+        transitionScript.enabled = false;
+    }
+	
+    protected void Correct()
+    {
+        correctSymbol.SetActive(true);
+        transitionScript.enabled = true;
+    }
+}
---
+++ Assets/Scripts/WaitForKeyboardInput.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitForKeyboardInput : ConditionScript
+{
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        if (!Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            return;
+        }
+
+        Correct();
+        enabled = false;
+    }
+    
+}
