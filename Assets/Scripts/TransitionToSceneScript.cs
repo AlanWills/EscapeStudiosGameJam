93e7e62382da1dde82ce770503d0ee116f30f2ab
Alan Wills <alawills@googlemail.com>
2017-06-11 00:42:26 +0100

    Working on extending time before wipe

--- Assets/Scripts/TransitionToSceneScript.cs
+++ Assets/Scripts/TransitionToSceneScript.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TransitionToSceneScript : MonoBehaviour {
-
+public class TransitionToSceneScript : MonoBehaviour
+{
+    public Animator WipeAnimator;
     public string SceneName;
     public float Countdown = 1.5f;
+    public float WipeCountdown = 1;
     public string AxisToTrigger;
 
     private float currentTime = 0;
+    private bool wiping = false;
 
 	// Use this for initialization
 	void Start () {
@@ -36,6 +39,18 @@ public class TransitionToSceneScript : MonoBehaviour {
 
     public void ForceTransition()
     {
+        if (!wiping && WipeAnimator != null)
+        {
+            currentTime = 0;
+            WipeAnimator.SetTrigger("wipe");
+            wiping = true;
+        }
+
+        if (currentTime < WipeCountdown)
+        {
+            return;
+        }
+
         SceneManager.LoadScene(SceneName);
     }
 }
