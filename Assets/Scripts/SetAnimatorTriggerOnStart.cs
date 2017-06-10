ca0e357b5117c15c675e501ff7cb6349ac2657d3
Alan Wills <alawills@googlemail.com>
2017-06-10 15:02:58 +0100

    Working on planes and subs level

--- Assets/Scripts/SetAnimatorTriggerOnStart.cs
+++ Assets/Scripts/SetAnimatorTriggerOnStart.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
-public class SetAnimatorTrigger : MonoBehaviour
+public class SetAnimatorTriggerOnStart : MonoBehaviour
 {
     public string TriggerName;
 
