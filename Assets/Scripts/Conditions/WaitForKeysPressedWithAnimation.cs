edaecb19e854eb123e821c054293e820a9f5e30d
Alan Wills <alawills@googlemail.com>
2017-06-10 13:26:15 +0100

    Added animations to happy sad level

---
+++ Assets/Scripts/Conditions/WaitForKeysPressedWithAnimation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Animator))]
+public class WaitForKeysPressedWithAnimation : WaitForKeysPressed
+{
+    public string TriggerOnSuccess;
+    
+    protected override void OnCorrect()
+    {
+        base.OnCorrect();
+
+        gameObject.GetComponent<Animator>().SetTrigger(TriggerOnSuccess);
+    }
+}
