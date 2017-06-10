943c8168e26fa572d90610c3f661da4a1ff9fc2e
Alan Wills <alawills@googlemail.com>
2017-06-10 19:01:32 +0100

    Finished chicken particle effect

--- Assets/Scripts/ChickenController.cs
+++ Assets/Scripts/ChickenController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(ParticleSystem))]
 public class ChickenController : MonoBehaviour
 {
     public Vector2 Forward;
     public Vector2 Left;
+    public ParticleSystem DeathParticleSystem;
+
+    private Animator chickenAnimator;
+    private bool moving = false;
     
     // Use this for initialization
     void Start ()
     {
-		
+        chickenAnimator = GetComponent<Animator>();
+        chickenAnimator.SetTrigger("idle");
+
+        DeathParticleSystem.Stop();
 	}
 	
 	// Update is called once per frame
@@ -22,40 +31,76 @@ public class ChickenController : MonoBehaviour
         {
             MoveLeft();
         }
-
-        if (Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             MoveRight();
         }
-
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            MoveBack();
-        }
-
-        if (Input.GetKey(KeyCode.DownArrow))
+        else if (Input.GetKey(KeyCode.UpArrow))
         {
             MoveForward();
         }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            MoveBack();
+        }
+        else
+        {
+            TryStop();
+        }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Collidable")
+        {
+            gameObject.GetComponentInChildren<SpriteRenderer>().enabled = false;
+            DeathParticleSystem.Emit(31);
+        }
+    }
+    
     private void MoveLeft()
     {
         transform.localPosition += new Vector3(Left.x, Left.y, 0) * Time.deltaTime;
+
+        TryRun();
     }
 
     private void MoveRight()
     {
         transform.localPosition += new Vector3(-Left.x, -Left.y, 0) * Time.deltaTime;
+
+        TryRun();
     }
 
     private void MoveForward()
     {
         transform.localPosition += new Vector3(Forward.x, Forward.y, 0) * Time.deltaTime;
+
+        TryRun();
     }
 
     private void MoveBack()
     {
         transform.localPosition += new Vector3(-Forward.x, -Forward.y, 0) * Time.deltaTime;
+
+        TryRun();
+    }
+
+    private void TryRun()
+    {
+        if (!moving)
+        {
+            moving = true;
+            chickenAnimator.SetTrigger("running");
+        }
+    }
+
+    private void TryStop()
+    {
+        if (moving)
+        {
+            moving = false;
+            chickenAnimator.SetTrigger("running");
+        }
     }
 }
---
+++ Assets/Scripts/Conditions/WaitForTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class WaitForTrigger : ConditionScript
+{
+    public string TagToCheckFor;
+
+    private bool collided = false;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == TagToCheckFor)
+        {
+            collided = true;
+        }
+    }
+
+    protected override bool Condition() { return collided; }
+}
