e0af8026c2de8c30f142b77ce2f93541a0cf7d4a
Alan Wills <alawills@googlemail.com>
2017-06-10 14:16:46 +0100

    Working on planes and subs level

---
+++ Assets/Scripts/SetAnimatorTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class SetAnimatorTrigger : MonoBehaviour
+{
+    public string TriggerName;
+
+	// Use this for initialization
+	void Start ()
+    {
+        GetComponent<Animator>().SetTrigger(TriggerName);
+	}
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float TimerLength = 10;
+
+    private float currentTimer = 0;
+
+    void Update()
+    {
+        currentTimer += Time.deltaTime;
+
+        if (currentTimer > TimerLength)
+        {
+            // Do something
+        }
+    }
+}
