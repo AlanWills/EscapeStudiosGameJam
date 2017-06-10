c6bb6936ede2da830ac968734cc4aa5041fd84d1
Alan Wills <alawills@googlemail.com>
2017-06-10 11:56:13 +0100

    Working on running man

--- Assets/Scripts/RunnerController.cs
+++ Assets/Scripts/RunnerController.cs
@@ -1,22 +1,70 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(Animator))]
 public class RunnerController : MonoBehaviour
 {
+    private enum Orientation
+    {
+        kUp,
+        kDown
+    }
+
     private Animator runnerAnimator;
+    private Orientation orientation = Orientation.kUp;
 
 	// Use this for initialization
 	void Start ()
     {
         runnerAnimator = GetComponent<Animator>();
         runnerAnimator.SetTrigger("running");
+
+        SetUp();
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            SetUp();
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            SetDown();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Collidable")
+        {
+            SceneManager.LoadScene("RunningManLevel");
+        }
+    }
+
+    private void SetUp()
+    {
+        if (orientation != Orientation.kUp)
+        {
+            Vector3 localPosition = transform.localPosition;
+            localPosition.y *= -1;
+            transform.localPosition = localPosition;
+            GetComponentInChildren<SpriteRenderer>().flipY = false;
+            orientation = Orientation.kUp;
+        }
+    }
+
+    private void SetDown()
+    {
+        if (orientation != Orientation.kDown)
+        {
+            Vector3 localPosition = transform.localPosition;
+            localPosition.y *= -1;
+            transform.localPosition = localPosition;
+            GetComponentInChildren<SpriteRenderer>().flipY = true;
+            orientation = Orientation.kDown;
+        }
+    }
 }
---
+++ Assets/Scripts/SidewaysScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class SidewaysScroller : MonoBehaviour
+{
+    public float Speed;
+    private Rigidbody2D objectRigidBody;
+
+	// Use this for initialization
+	void Start ()
+    {
+        objectRigidBody = GetComponent<Rigidbody2D>();
+        objectRigidBody.velocity = new Vector2(Speed, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
