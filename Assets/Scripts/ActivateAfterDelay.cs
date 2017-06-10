0b7cb9c5700b691148f5bf469239394861087b7a
Alan Wills <alawills@googlemail.com>
2017-06-10 21:06:22 +0100

    Working on fixing script initialization error

---
+++ Assets/Scripts/ActivateAfterDelay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateAfterDelay : MonoBehaviour
+{
+    public float Delay = 1;
+    public Behaviour Behaviour;
+
+    private float currentTimer = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Behaviour.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        currentTimer += Time.deltaTime;
+        if (currentTimer > Delay)
+        {
+            Behaviour.enabled = true;
+            enabled = false;
+        }
+	}
+}
