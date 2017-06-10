0a46e9cca05c22dc21142f02933d0fd105222f52
Alan Wills <alawills@googlemail.com>
2017-06-10 11:31:01 +0100

    Working on runner level

---
+++ Assets/Scripts/RunnerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class RunnerController : MonoBehaviour
+{
+    private Animator runnerAnimator;
+
+	// Use this for initialization
+	void Start ()
+    {
+        runnerAnimator = GetComponent<Animator>();
+        runnerAnimator.SetTrigger("running");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
