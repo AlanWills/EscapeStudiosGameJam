6011f00de3ac5602ff0cf65c5853de6d70665193
Alan Wills <alawills@googlemail.com>
2017-06-10 12:31:00 +0100

    Working on happy sad level and refactoring code

---
+++ Assets/Scripts/Conditions/WaitForKeysPressed.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitForKeysPressed : ConditionScript
+{
+    public List<KeyCode> Keys = new List<KeyCode>();
+
+    private Dictionary<KeyCode, bool> keysPressed = new Dictionary<KeyCode, bool>();
+
+    public WaitForKeysPressed()
+    {
+        foreach (KeyCode key in Keys)
+        {
+            keysPressed.Add(key, false);
+        }
+    }
+
+    // Update is called once per frame
+    public override void Update ()
+    {
+        base.Update();
+
+        foreach (KeyCode key in Keys)
+        {
+            bool keyAlreadyPressed = keysPressed[key];
+            keysPressed[key] = keyAlreadyPressed || Input.GetKeyDown(key);
+        }
+    }
+    
+    protected override bool Condition()
+    {
+        foreach (KeyCode key in Keys)
+        {
+            if (!keysPressed[key])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
