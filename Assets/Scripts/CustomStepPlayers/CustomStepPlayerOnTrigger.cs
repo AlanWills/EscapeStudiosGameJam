b45bf45d5a254c42954242ec82b894620adaa920
Alan Wills <alawills@googlemail.com>
2017-06-10 22:56:49 +0100

    Changed when audio is played in hot and cold level

---
+++ Assets/Scripts/CustomStepPlayers/CustomStepPlayerOnTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class CustomStepPlayerOnTrigger : CustomStepPlayer
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlaySteps();
+    }
+}
