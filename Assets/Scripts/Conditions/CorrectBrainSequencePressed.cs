1c8315d55b6e30a33364d54e201fcfc239b17106
Alan Wills <alawills@googlemail.com>
2017-06-10 20:28:57 +0100

    Finished left and right brain level

---
+++ Assets/Scripts/Conditions/CorrectBrainSequencePressed.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class CorrectBrainSequencePressed : ConditionScript
+{
+    public Animator PlayerAnimator;
+
+    private Animator brainAnimator;
+    bool upPressed = false;
+    bool leftPressed = false;
+    bool downPressed = false;
+
+    public override void Start()
+    {
+        base.Start();
+
+        brainAnimator = GetComponent<Animator>();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (Condition())
+        {
+            // Don't allow input changes when complete
+            return;
+        }
+
+        if (!upPressed)
+        {
+            // Nothing in the sequence is pressed
+
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                upPressed = true;
+                brainAnimator.SetTrigger("next");
+            }
+        }
+        else if (!leftPressed)
+        {
+            // Up pressed, but nothing else
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                leftPressed = true;
+                brainAnimator.SetTrigger("next");
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                upPressed = false;
+                brainAnimator.SetTrigger("previous");
+            }
+        }
+        else if (!downPressed)
+        {
+            // Up and left pressed, but not down
+
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                downPressed = true;
+                brainAnimator.SetTrigger("next");
+                PlayerAnimator.SetTrigger("clever");
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                leftPressed = false;
+                brainAnimator.SetTrigger("previous");
+            }
+        }
+    }
+
+    protected override bool Condition()
+    {
+        return upPressed && leftPressed && downPressed;
+    }
+}
---
+++ Assets/Scripts/CustomSteps/SetVolume.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetVolume : CustomStep
+{
+    public float Volume;
+    public AudioSource AudioSource;
+
+    public override void OnSuccess()
+    {
+        AudioSource.volume = Volume;
+    }
+
+    public override void OnFailure()
+    {
+    }
+}
