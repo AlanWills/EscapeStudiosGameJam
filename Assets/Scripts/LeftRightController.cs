1c9e7c5f50b77062f19aa446395ea6a4bd187955
Alan Wills <alawills@googlemail.com>
2017-06-10 22:43:06 +0100

    Finished hot and cold level

---
+++ Assets/Scripts/LeftRightController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftRightController : MonoBehaviour
+{
+    public float Speed = 1;
+
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            MoveLeft();
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            MoveRight();
+        }
+	}
+
+    private void MoveLeft()
+    {
+        transform.localPosition += new Vector3(-Speed * Time.deltaTime, 0, 0);
+    }
+
+    private void MoveRight()
+    {
+        transform.localPosition += new Vector3(Speed * Time.deltaTime, 0, 0);
+    }
+}
