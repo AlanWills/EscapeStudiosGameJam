5ee04bbc64ef93583d5f1f9d1db410e96220c11f
Alan Wills <alawills@googlemail.com>
2017-06-11 11:04:49 +0100

    Black and white level fixes

--- Assets/Scripts/CustomStepPlayers/CustomStepPlayerOnKeyPress.cs
+++ Assets/Scripts/CustomStepPlayers/CustomStepPlayerOnKeyPress.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class CustomStepPlayerOnKeyPress : CustomStepPlayer
 {
+    public List<KeyCode> Keys = new List<KeyCode>();
+
 	// Update is called once per frame
 	void Update ()
     {
-		if (Input.anyKeyDown)
+		if (Keys.Count == 0 && Input.anyKeyDown)
+        {
+            PlaySteps();
+        }
+        else if (Keys.Exists(x => Input.GetKeyDown(x)))
         {
             PlaySteps();
         }
